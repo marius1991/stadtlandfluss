47b6b4d3e6911b1ff8322b0346ecb1a2e6bbdf4b
marius1991 <marius.h@gmx.de>
2015-02-24 12:39:56 +0100

    Design angepasst

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-    flash[:success] = "Profile updated"
+    flash[:success] = "Profil bearbeitet"
     redirect_to root_url
   else
       render 'edit'
