ff8386812a27a4a87782b886b894b835e3494f94
marius1991 <marius.h@gmx.de>
2015-02-24 17:38:30 +0100

    kleine Änderung

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ActiveRecord::Base
   has_many :scores
 
   validates :name ,
-            :presence => {:message => "cant be blank"},
+            :presence => {:message => "darf nicht leer sein"},
             :format =>{ :with => /\A[a-zA-Z0-9]+\Z/,
-                        :message => ": No blankspace allowed!"},
+                        :message => "darf keine Leerzeichen enthalten"},
             on: :create, on: :update
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, on: :update
-  has_secure_password :message => "darf nicht leer sein!"
-  validates_presence_of :password, :on => :create, on: :update
-  validates_presence_of :password_confirmation, :on => :create, on: :update
+  has_secure_password :message => ": darf nicht leer sein!"
+  validates_presence_of :password, :on => :create, on: :update, :message => "darf nicht leer sein"
+  validates_presence_of :password_confirmation, :on => :create, on: :update, :message => "darf nicht leer sein"
 
 
 
