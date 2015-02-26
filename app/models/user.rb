d4919e5dc608e54943d1ffc5c5cdd929f77700b1
marius1991 <marius.h@gmx.de>
2015-02-26 17:54:31 +0100

    Devise final

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
   # attr_accessor :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username ,
+            :presence => {:message => "darf nicht leer sein"},
+            :format =>{ :with => /\A[a-zA-Z0-9]+\Z/,
+                        :message => "darf keine Leerzeichen enthalten"},
+            on: :create, on: :update
+
 end
